940f9b8ddfac3701151bb9ddfd014e7be42c0dcd
Letjobana <suzanm@gerbertech.co.za>
2022-02-01 21:18:56 +0200

    Updated the startup and appsetting.json to add a database connection

--- InspectionAPI/InspectionAPI/Startup.cs
+++ InspectionAPI/InspectionAPI/Startup.cs
@@ -1,7 +1,9 @@
+using InspectionAPI.Persistance;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,7 @@ namespace InspectionAPI
         {
 
             services.AddControllers();
+            services.AddDbContext<DataContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "InspectionAPI", Version = "v1" });
