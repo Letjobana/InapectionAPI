235033dfdd71e17d43da14536e595372ca5f6cac
Letjobana <suzanm@gerbertech.co.za>
2022-02-01 21:41:35 +0200

    Added Status Controller

---
+++ InspectionAPI/InspectionAPI/Controllers/InspectionTypesController.cs
@@ -0,0 +1,110 @@
+﻿using InspectionAPI.Models;
+using InspectionAPI.Repository.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace InspectionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InspectionTypesController : ControllerBase
+    {
+        private readonly IInspectionTypeRepository inspectionTypeRepository;
+
+        public InspectionTypesController(IInspectionTypeRepository inspectionTypeRepository)
+        {
+            this.inspectionTypeRepository = inspectionTypeRepository;
+        }
+        [HttpGet]
+        public IActionResult GetAllInspectionTypes()
+        {
+            try
+            {
+                return Ok(inspectionTypeRepository.GetAllInspectionTypes().ToList());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetAllInspectionTypessById(int id)
+        {
+            try
+            {
+                var inspectionTypeId = inspectionTypeRepository.GetInspectionTypeById(id);
+                if (inspectionTypeId == null)
+                    return NotFound();
+                return Ok(inspectionTypeId);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                     "Invalid Inspection Type,please enter a valid inspection");
+            }
+        }
+        [HttpPost]
+        public IActionResult AddInspectionType(InspectionType inspectionType)
+        {
+            try
+            {
+                if (inspectionType == null)
+                    return BadRequest();
+                var addInspectionType = inspectionTypeRepository.AddInspectionType(inspectionType);
+                return CreatedAtAction(nameof(GetAllInspectionTypessById),
+                   new { id = addInspectionType.Id }, addInspectionType);
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                     "Error when adding a new status");
+            }
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateStatus(int id, InspectionType inspectionType)
+        {
+            try
+            {
+                if (id != inspectionType.Id)
+                    return BadRequest("ProductId Mismatched");
+
+                var inspectionId = inspectionTypeRepository.GetInspectionTypeById(id);
+                if (inspectionId == null)
+                    return NotFound($"Product with Id = {id} not found");
+
+                return Ok(inspectionTypeRepository.UpdateInspectionType(inspectionType));
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error updating data");
+            }
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteInspectionType(int id)
+        {
+            try
+            {
+                var statusId = inspectionTypeRepository.GetInspectionTypeById(id);
+                if (statusId == null)
+                    return NotFound();
+                return Ok(inspectionTypeRepository.DeleteInspectionType(id));
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                     "Error when deleting a the inspection type,please try again");
+            }
+
+        }
+    }
+}
---
+++ InspectionAPI/InspectionAPI/Controllers/StatusController.cs
@@ -0,0 +1,110 @@
+﻿using InspectionAPI.Models;
+using InspectionAPI.Repository.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace InspectionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly IStatusRepository statusRepository;
+
+        public StatusController(IStatusRepository statusRepository)
+        {
+            this.statusRepository = statusRepository;
+        }
+        [HttpGet]
+        public IActionResult GetAllStatus()
+        {
+            try
+            {
+                return Ok(statusRepository.GetAllStatuses().ToList());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetAllStatusById(int id)
+        {
+            try
+            {
+                var statusId = statusRepository.GetStatusById(id);
+                if (statusId == null)
+                    return NotFound();
+                return Ok(statusId);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                     "Invalid status,please enter a valid status");
+            }
+        }
+        [HttpPost]
+        public IActionResult AddStatus(Status status)
+        {
+            try
+            {
+                if (status == null)
+                    return BadRequest();
+                var addStatus = statusRepository.AddStatus(status);
+                return CreatedAtAction(nameof(GetAllStatusById),
+                   new { id = addStatus.Id }, addStatus);
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                     "Error when adding a new status");
+            }
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateStatus(int id, Status status)
+        {
+            try
+            {
+                if (id != status.Id)
+                    return BadRequest("ProductId Mismatched");
+
+                var statusId = statusRepository.GetStatusById(id);
+                if (statusId == null)
+                    return NotFound($"Product with Id = {id} not found");
+
+                return Ok(statusRepository.UpdateStatus(status));
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error updating data");
+            }
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteStatus(int id)
+        {
+            try
+            {
+                var statusId = statusRepository.GetStatusById(id);
+                if (statusId == null)
+                    return NotFound();
+                return Ok(statusRepository.DeleteStatus(id));
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                     "Error when deleting a status,please try again");
+            }
+
+        }
+    }
+}
